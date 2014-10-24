80c91510ee635925e424079eb6666010b5821e9a
CJ Strohofer <strohs1@gmail.com>
2014-10-23 20:04:59 -0400

    initial commit plus project 2

---
+++ src/euler/problem2/core.clj
@@ -0,0 +1,6 @@
+(ns euler.problem2.core)
+
+;; uses +' to promote to BigInt
+(def fib (map first (iterate (fn [[a b]] [b (+' a b)]) [1 2])))
+
+(apply + (filter even? (take-while #(< % 4000000) fib)))
